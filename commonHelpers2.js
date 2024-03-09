adb033d0f182b369d005a1f7f959b35ac232a448
davydenko919 <141831028+davydenko919@users.noreply.github.com>
2024-03-09 20:18:39 +0000

    Deploying to gh-pages from @ davydenko919/goit-js-hw-11@64400c549057b632749fc037f25cac09b7e9c4fe 🚀

--- commonHelpers2.js
+++
@@ -1,2 +0,0 @@
-import"./assets/styles-14f7ea0f.js";import{i as s}from"./assets/vendor-77e16229.js";const m=document.querySelector(".input-ms"),n=document.querySelector(".state-true"),o=document.querySelector(".form");o.addEventListener("submit",r=>{r.preventDefault();const t=m.value,i=n.checked;new Promise((e,c)=>{setTimeout(()=>{i?e(t):c(t)},t)}).then(e=>{s.success({message:`✅ Fulfilled promise in ${e}ms`})}).catch(e=>{s.error({message:`❌ Rejected promise in ${e}ms`})}),o.reset()});
-//# sourceMappingURL=commonHelpers2.js.map

bc811cfa5a9fab455076a753797511a02d97ada6
davydenko919 <davydenko919@gmail.com>
2024-03-11 12:22:58 +0200

    ssss

--- src/main.js
+++ src/main.js
@@ -6,22 +6,12 @@ import {createGallery} from "./js/render-functions.js"
 const cardPlace = document.querySelector('.card-place');
 const form = document.querySelector('.form');
 const input = form.querySelector('.input');
-// const QUERY = input.value.trim().toLowerCase().replace(/ /g, "+");
-
-// const KEY = `42787384-4c627c93f7dff570902230658`;
-// const BASE_URI = `https://pixabay.com/api/`;
-
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
 
     cardPlace.innerHTML = '';
     const QUERY = input.value.trim().toLowerCase().replace(/ /g, "+");
-    // const what = `${BASE_URI}?key=${KEY}&q=${QUERY}&image_type=photo&orientation=horizontal&savesearch=true`;
-    
-    // console.log(what)
-    // console.log(getImages(QUERY));
-    // console.log(createGallery(getImages(QUERY)));
     getImages(QUERY)
     .then(arr => {
         cardPlace.innerHTML = createGallery(arr);
@@ -29,7 +19,10 @@ form.addEventListener('submit', (event) => {
         form.reset();
       })
       .catch(error => {
-        console.error('Error:', error);
+        iziToast.error({
+          message:
+            `${error}`,
+        });
       });
     }
 );
